b9f7d41ddad380c3afb7920955060f7c614833a8
njefferis1 <46577146+njefferis1@users.noreply.github.com>
2019-09-12 10:18:07 -0600

    added entity classes

--- src/Blogging Demo - Code First/BloggingDemo/Program.cs
+++ src/Blogging Demo - Code First/BloggingDemo/Program.cs
@@ -12,4 +12,29 @@ namespace BloggingDemo
         {
         }
     }
+
+    namespace Enitities
+    {
+        public class Blog
+        {
+            // "Primary Key", by convention
+            public int BlogID { get; set; }
+            public string Name { get; set; }
+
+            // Navigation Property
+            public virtual ICollection<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            // "Primary Key", by convention
+            public int PostID { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public int BlogID { get; set; }
+
+            // Navigation Property
+            public virtual Blog Blog { get; set; }
+        }
+    }
 }
